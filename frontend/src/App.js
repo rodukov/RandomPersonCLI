a4a9d18336fea38e5d98be4f7e5caefbcf46b898
rodukov <electroneuphoria@gmail.com>
2022-08-01 17:59:06 +0300

    to the deploy?!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,21 @@
 import './styles/App.css'
 import { Generate } from './components/generate'
 function App() {
-  document.title = "Generated 6 peoples"
+  document.title = "Generated 2 peoples"
   return (
+    <div>
+    <p className="welc">Human Generator</p>
+    <p className="welcgg">Choice people which more likes for you</p>
     <div className="App">
-        {Generate(6).map(human => {
+        {Generate(2).map(human => {
           return <div className="FakeHuman">
-            <img src="https://thispersondoesnotexist.com/image" className="person_image" alt="this_is_human_do_not_exists"></img>
+            <p className="center"><img src="https://thispersondoesnotexist.com/image" className="person_image" alt="this_is_human_do_not_exists"></img></p>
             <div className="data">
               {human.map(dataitem => <p className="paragraph">{dataitem}</p> )}
             </div>
           </div>
         })}
+    </div>
     </div>
   );
 }
--- frontend/src/components/generate.js
+++ frontend/src/components/generate.js
@@ -5,10 +5,16 @@ export function Generate(humans) {
     let human = 0
     let output = []
     while (human < humans) {
-        let name = faker.name.findName()
-        let email = faker.internet.email()
-        let address = faker.address.streetAddress()
-        output.push([name, email, address])
+        output.push([
+            `${faker.name.firstName()} ${faker.name.lastName()}`,
+            faker.name.gender(),
+            faker.name.jobTitle(),
+            faker.phone.number('+###########'),
+            `Favourite color:  ${faker.commerce.color()}`,
+            `Genres: ${faker.music.genre()} and ${faker.music.genre()}`,
+            `EMEI: ${faker.phone.imei()}`,
+            `Selector: ${faker.word.adverb()}`,
+        ])
         human += 1
     }
     return output
